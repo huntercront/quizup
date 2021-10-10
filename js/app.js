e621936a7fbadde7a5db6b59dca7f9919ee07204
huntercront <38252870+huntercront@users.noreply.github.com>
2021-10-10 14:01:53 +0300

    add page

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ WebFontConfig = {
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    document.querySelector('body').classList.remove('loading')
+    document.querySelector('body').classList.remove('loading');
     let introSteps = document.querySelectorAll('[to-intro]');
     introSteps.forEach(function(introStep) {
         introStep.addEventListener('click', function(e) {
@@ -23,5 +23,77 @@ document.addEventListener("DOMContentLoaded", function(event) {
             nextIntro.classList.add('active')
         })
     })
+
+    if (document.querySelector('.question')) {
+        let progresBar = document.querySelector('.hs-circle--bar');
+        let totalTime = parseInt(document.querySelector('.total-time').textContent);
+        let curentTime = document.querySelector('.curent-time');
+        let timeCounter = totalTime;
+        let stage = parseInt(document.querySelector('.question').getAttribute('stage'));
+        let lives = parseInt(document.querySelector('.question').getAttribute('lives'));
+        let totalLiver = document.querySelectorAll('.live').length;
+        let canSelect = document.querySelector('.can-select');
+        var audio = new Audio('../mp3/chasyi-obratnogo-otscheta-medlennogo-vrascheniya-38140 (mp3cut.net).mp3');
+
+
+        function updateLives() {
+            if (lives > 1) {
+                document.querySelector('[live-count="' + (totalLiver - lives + 1) + '"]').classList.add('remove-life');
+            }
+        }
+
+        function updateBar(value) {
+            let partUpdate = 80 / totalTime;
+            progresBar.style.strokeDashoffset = 100 - (partUpdate * (totalTime - value + 1));
+        }
+        let timerId = setInterval(function() {
+            timeCounter--;
+            if (timeCounter == 5) {
+                curentTime.classList.add('will-end');
+                audio.play();
+            }
+            updateBar(timeCounter);
+            curentTime.textContent = timeCounter;
+            if (timeCounter == 0) {
+                clearTimeout(timerId);
+                curentTime.classList.remove('will-end');
+                audio.pause();
+                setTimeout(function() {
+                    checkAnswer();
+                    updateLives();
+                }, 200);
+
+            }
+        }, 1000);
+
+        function checkAnswer() {
+            canSelect.classList.remove('can-select');
+            let ansverRight = document.querySelector('.question').getAttribute('key');
+            if (document.querySelector('.ansver.selected')) {
+                if (('answer-' + ansverRight) == document.querySelector('.ansver.selected').id) {
+                    document.querySelector('.ansver.selected').classList.add('ansver-right')
+                } else {
+                    document.querySelector('.ansver.selected').classList.add('ansver-wrong');
+                    document.getElementById('answer-' + ansverRight).classList.add('ansver-right');
+                }
+            } else {
+                document.getElementById('answer-' + ansverRight).classList.add('ansver-right')
+            }
+        }
+
+
+        let ansvers = document.querySelectorAll('.ansver');
+        ansvers.forEach(function(ansver) {
+            ansver.addEventListener('click', function(e) {
+                if (canSelect.classList.contains('can-select')) {
+                    if (document.querySelector('.ansver.selected')) {
+                        document.querySelector('.ansver.selected').classList.remove('selected')
+                    }
+                    ansver.classList.add('selected')
+                }
+            })
+        })
+    }
+
 
 })
